ce56bd977b8218bbe653afc685637b6e18241f01
Zasbir <140260801+Zasbir@users.noreply.github.com>
2024-08-04 18:46:24 +0100

    Add files via upload

---
+++ wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P00c0e12
+                 \e]P158565E
+                 \e]P2996459
+                 \e]P3C66A5D
+                 \e]P4BA9371
+                 \e]P56E7186
+                 \e]P69B709F
+                 \e]P7a6c6ca
+                 \e]P8748a8d
+                 \e]P958565E
+                 \e]PA996459
+                 \e]PBC66A5D
+                 \e]PCBA9371
+                 \e]PD6E7186
+                 \e]PE9B709F
+                 \e]PFa6c6ca
+                 \ec'
---
+++ wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#a6c6ca", "#0c0e12" },
+	[SchemeSel] = { "#a6c6ca", "#58565E" },
+	[SchemeOut] = { "#a6c6ca", "#9B709F" },
+};
